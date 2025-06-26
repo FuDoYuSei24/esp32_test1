53840b1f7494e3d50e7b88e7d664aefed9625f6c
FuDoYuSei24 <2935155425@qq.com>
2025-06-26 23:24:31 +0800

    first_save

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+#define TRIG 32
+#define ECHO 33
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(TRIG,OUTPUT);
+  pinMode(ECHO,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(TRIG,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG,LOW);
+
+  double delta_time = pulseIn(ECHO,HIGH);
+  float detect_distance = delta_time * 0.0343/2;
+  Serial.printf("distande = %f cm\n",detect_distance);
+  delay(100);
+
+}
+
+
+
